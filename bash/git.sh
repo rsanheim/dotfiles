a21d90d793d2590fd1aa14d44f0d206e5ff53c86
Rob Sanheim <rsanheim@gmail.com>
2015-10-23 22:54:41 -0500

    aliases

---
+++ bash/git.sh
@@ -0,0 +1,25 @@
+# Thanks to Geoffrey's peepcode for many of these
+alias g='git '
+alias gst='git status'
+alias gap='git add --patch'
+alias gs='git status'
+alias gl='git pull'
+alias gp='git push'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gcap='git commit -v -a && git push'
+alias gpp='git pull; git push'
+
+# From: http://www.commandlinefu.com/commands/view/2345/show-git-branches-by-date-useful-for-showing-active-branches
+alias "git-recent-branches"='for k in `git branch|perl -pe s/^..//`;do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k|head -n 1`\\t$k;done|sort -r'
+alias grb="git-recent-branches"
+
+# Throw a timestamp in README.markdown and commit and push
+alias random_commit="echo `date` >> README.markdown \
+  && git ci -am 'random commit' \
+  && git push"
+
+alias "clean-local-branches"="git branch --merged master | grep -v 'master$' | xargs git branch -d"
+alias "git-sync"="git co master && git pull && clean-local-branches"
