65dd7c4d755d30699b938fef2b7a5abc5933c51b
Rob Sanheim <rsanheim@gmail.com>
2015-10-25 13:03:26 -0500

    bootstrap vundler

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,8 @@ ln -sf ~/src/dotfiles/.bash_profile ~
 ln -sf ~/src/dotfiles/.vimrc ~
 source ~/.bash_profile
 brew bundle
+# setup Vundle for vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install plugins via vim + vundle
+vim +PluginInstall +qall
 echo "==> All done!"
