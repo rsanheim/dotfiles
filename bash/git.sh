d694a49887667053f834f79fd47817c6443bfbf1
Rob Sanheim <rsanheim@gmail.com>
2016-07-12 13:35:59 -0500

    recent for remote

--- bash/git.sh
+++ bash/git.sh
@@ -14,6 +14,7 @@ alias gpp='git pull; git push'
 
 # From: http://www.commandlinefu.com/commands/view/2345/show-git-branches-by-date-useful-for-showing-active-branches
 alias "git-recent-branches"='for k in `git branch|perl -pe s/^..//`;do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k|head -n 1`\\t$k;done|sort -r'
+alias "git-recent-branches-remote"='for k in `git branch -r|head 10|perl -pe s/^..//`;do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k|head -n 1`\\t$k;done|sort -r'
 alias grb="git-recent-branches"
 
 # Throw a timestamp in README.markdown and commit and push
