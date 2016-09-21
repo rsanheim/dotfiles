9d86890bdb57f3705c4d9fae79ef71a6a683f0ce
Rob Sanheim <rsanheim@gmail.com>
2016-09-21 14:27:57 -0500

    update t to use the better script

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -12,15 +12,15 @@ alias mix="cd ~/src/mixcatcher"
 alias "edit-bash-profile"="vim /Users/rsanheim/src/dotfiles/.bash_profile"
 alias "reload-bash-profile"=". ~/.bash_profile"
 
-alias pr="script/git-pr --push"
-# rails
+# rails and github stuff
 alias sb="script/bootstrap"
 alias sc="script/console"
 alias ss="script/server"
-alias t="bin/testrb"
+alias t="bin/testrb_or_zt"
 alias z="GH_ZEUS=1 bin/zeus"
 alias pr="script/git-pr --push"
 alias beg="bin/bundle exec guard"
+alias pr="script/git-pr --push"
 
 alias use_gcc="export CC=/usr/bin/gcc && export CXX=/usr/bin/g++ && export LD=/usr/bin/gcc"
 alias "count-github-test-code"="sloccount test/models test/integration test/controllers test/jobs test/lib test/mailers test/view_models"
