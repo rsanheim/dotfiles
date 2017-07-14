c909288daf3d998c88b458547ad44182619b5fba
Rob Sanheim <rsanheim@gmail.com>
2017-07-14 00:40:12 -0500

    fix alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,7 +7,7 @@ alias rdm="bundle exec rake db:migrate"
 alias rdt="bin/rake db:migrate db:test:prepare"
 alias m="mvim ."
 alias mix="cd ~/src/mixcatcher"
-alias nb="cd ~/src/nationbuilder/nbuild"
+alias nb="cd ~/src/nbuild"
 
 # just make life easier
 alias "edit-bash-profile"="vim /Users/rsanheim/src/dotfiles/.bash_profile"
