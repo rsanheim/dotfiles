740b03596876a57f0c1c0942f32015e8794e804d
Rob Sanheim <rsanheim@gmail.com>
2017-07-25 22:05:36 -0500

    add install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,10 @@ if is_osx; then
   brew bundle
 fi
 
+if is_ubuntu_desktop; then
+  bin/install-xmr-stak-amd
+fi
+
 # setup Vundle for vim
 git -C ~/.vim/bundle/Vundle.vim pull || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
