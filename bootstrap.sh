3ac3a1434d6cad353211001e0bbe3bc94c4e4015
Rob Sanheim <rsanheim@gmail.com>
2017-08-08 13:22:07 -0500

    xmr stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,6 +14,16 @@ if is_osx; then
   brew bundle
 fi
 
+if is_ubuntu; then
+  sudo sysctl -w vm.nr_hugepages=128
+  git clone https://github.com/fireice-uk/xmr-stak-cpu.git $HOME/src/xmr-stak-cpu
+  pushd $HOME/src/xmr-stak-cpu
+  sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+  cmake .
+  make install
+fi
+
+
 if is_ubuntu_desktop; then
   bin/install-amdgpu-pro
   bin/install-xmr-stak-amd
