6fae80f3e266947d13dffccbde1e4782f9ccb5a5
Rob Sanheim <rsanheim@gmail.com>
2017-08-10 11:53:19 -0500

    setup docs as symlink to Dropbox docs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,13 +8,25 @@ ln -sf ~/src/dotfiles/.vimrc ~
 ln -sf ~/src/dotfiles/.gitconfig ~
 ln -sf ~/src/dotfiles/.atom/ ~/.atom
 
-git clone https://github.com/rsanheim/dotfiles-private.git $HOME/src/dotfiles-private
+if [[ ! -d $HOME/src/dotfiles-private ]]; then
+  git clone https://github.com/rsanheim/dotfiles-private.git $HOME/src/dotfiles-private
+fi
+
+if [[ ! -L "$HOME/Documents" && -d "$HOME/Dropbox/Documents" ]]; then
+  echo "Symlinking Dropbox documents to $HOME/Documents"
+  if [[ -d "$HOME/Documents" ]]; then
+    sudo mv $HOME/Documents $HOME/Documents.old
+  fi
+  ln -sv $HOME/Dropbox/Documents $HOME
+fi
+
 
 if is_osx; then
   brew bundle
 fi
 
 if is_ubuntu; then
+  # install xmr-stak-cpu
   sudo sysctl -w vm.nr_hugepages=128
   git clone https://github.com/fireice-uk/xmr-stak-cpu.git $HOME/src/xmr-stak-cpu
   pushd $HOME/src/xmr-stak-cpu
