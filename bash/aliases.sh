11889aba22ca1a06653aba3730dce827ccaaf180
Rob Sanheim <rsanheim@gmail.com>
2020-04-28 10:31:25 -0500

    more cleanup and aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -9,16 +9,14 @@ alias rdm="bundle exec rake db:migrate"
 alias rdt="bin/rake db:migrate db:test:prepare"
 alias m="mvim ."
 alias mix="cd ~/src/mixcatcher"
+alias ss="cd ~/src/simpledotorg/simple-server"
 
 # just make life easier
-alias "edit-bash-profile"="vim $DOTFILES_HOME/.bash_profile"
+alias "edit-bash-profile"="code $DOTFILES_HOME/"
 alias "reload-bash-profile"=". ~/.bash_profile"
 alias "preflight"="~/src/rsanheim/preflight/preflight.rb"
 
 # rails stuff
-alias sb="script/bootstrap"
-alias sc="script/console"
-alias ss="script/server"
 alias beg="bin/bundle exec guard"
 
 alias use_gcc="export CC=/usr/bin/gcc && export CXX=/usr/bin/g++ && export LD=/usr/bin/gcc"
