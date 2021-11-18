be5aed8aac4071008ef2bd520b921e435065fef3
Rob Sanheim <rsanheim@gmail.com>
2021-11-17 21:22:09 -0600

    making things simpler

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,21 +14,27 @@ ln -sf $DOTFILES_HOME/.gitconfig ~
 ln -sf $DOTFILES_HOME/.atom/ ~
 ln -sf $DOTFILES_HOME/.gitignore ~
 
-if [[ ! -d $HOME/src/rsanheim/dotfiles-private ]]; then
-  git clone https://github.com/rsanheim/dotfiles-private.git $HOME/src/rsanheim/dotfiles-private
-fi
-
-ln -sf $DOTFILES_PRIVATE_HOME/ssh_config ~/.ssh/config
-
-if [[ ! -d $HOME/src/rsanheim/preflight ]]; then
-  git clone https://github.com/rsanheim/preflight.git $HOME/src/rsanheim/preflight
+if [[ ! -d $HOME/src/rsanheim ]]; then
+  echo "SSH'ing to GitHub to setup .ssh dir (this is expected to fail...)"
+  ssh git@github.com
 fi
 
 pushd "$HOME/src/rsanheim"
-curl -s https://api.github.com/users/rsanheim/repos | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+if [[ ! -d $HOME/src/rsanheim ]]; then
+  echo "cloning personal repos..."
+  curl -s https://api.github.com/users/rsanheim/repos | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+fi
 popd
 
+ln -sf $DOTFILES_PRIVATE_HOME/ssh_config ~/.ssh/config
+
+if [[ ! -d $HOME/src/simpledotorg ]]; then
+  echo "cloning simple-server..."
+  ./bin/install-simple
+fi
+
 if is_osx; then
+  echo "installing brew bundle..."
   pushd $DOTFILES_HOME
   brew bundle
   popd
@@ -37,10 +43,10 @@ fi
 # Install nvm and install latest 12.x LTS version of Node
 
 # setup Vundle for vim
-git -C ~/.vim/bundle/Vundle.vim pull || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git -C ~/.vim/bundle/Vundle.vim pull || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install plugins via vim + vundle
-vim +PluginInstall +qall
+# vim +PluginInstall +qall
 
 source ~/.bashrc
 
